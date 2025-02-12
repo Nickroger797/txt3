1648c0c1a019f74028f0abac45c10431f0990101
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-12 17:56:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -118,10 +118,3 @@ if __name__ == "__main__":
 
     asyncio.get_event_loop().run_until_complete(main())
     LOGGER.info(f"<---Bot Stopped--->")
-
-
-if __name__ == "__main__":
-    #import os ( above )
-    port = int(os.environ.get('PORT', 5000))
-    app = Bot()
-    app.run(host='0.0.0.0', port=port)
